dd1b8ff4a9389e6fe12cbdef2390109241574a68
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-06-25 12:29:16 -0400

    form update6

--- app.vue
+++ app.vue
@@ -1,4 +1,18 @@
 <template>
-<h1>Hello</h1>
+<form name="contact" method="POST" data-netlify="true" action="/thank-you">
+  <input type="hidden" name="form-name" value="contact">
+
+  <label for="name">Name:</label>
+  <input type="text" id="name" name="name" required>
+
+  <label for="email">Email:</label>
+  <input type="email" id="email" name="email" required>
+
+  <label for="message">Message:</label>
+  <textarea id="message" name="message" required></textarea>
+
+  <button type="submit">Submit</button>
+</form>
+
 </template>
 
