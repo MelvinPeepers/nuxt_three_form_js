386bd9fe7a3c8235b8f38fdbb5ce70f48e479674
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-06-25 11:54:18 -0400

    form update

--- app.vue
+++ app.vue
@@ -2,19 +2,25 @@
   <div>
 
     <form name="contactone" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="contact">
+
+      <input type="hidden" name="form-name" value="contactone">
+
       <p>
-    <label>Your Name: <input type="text" name="name" /></label>
+      <label>Your Name: <input type="text" name="name" /></label>
       </p>
+
       <p>
         <label>Your Email: <input type="email" name="email" /></label>
       </p>
+
       <p>
         <label>Message: <textarea name="message"></textarea></label>
       </p>
+
       <p>
         <button type="submit">Send</button>
       </p>
+
     </form>
 
     <!-- <NuxtWelcome /> -->
