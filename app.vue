367fd52a9754bfd7217b193e6bf3b34cea5c24ef
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-06-25 11:42:05 -0400

    form

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
 <template>
   <div>
-    <NuxtWelcome />
+
+    <form name="contactone" method="POST" data-netlify="true">
+      <input type="hidden" name="form-name" value="contact">
+      <p>
+    <label>Your Name: <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Your Email: <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <label>Message: <textarea name="message"></textarea></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
+
+    <!-- <NuxtWelcome /> -->
   </div>
 </template>
